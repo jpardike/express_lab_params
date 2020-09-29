ccc2c7d52d34bf018b2838420d3c92da6e5a7bfe
jpardike2 <jpardike@gmail.com>
2020-09-29 11:45:28 -0700

    Commit 2: Tip Calculator express application created.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express');
 const express = require('express');
 const app = express();
 
@@ -7,6 +8,16 @@ app.get('/greeting', (request, response) => {
 
 app.get('/greeting/:name', (request, response) => {
     response.send(`Hello, ${request.params.name}`);
+});
+
+app.get('/tip/:total/:tipPercentage', (request, response) => {
+    const total = request.params.total;
+    const tipPercentage = request.params.tipPercentage;
+    const num = Number('.' + tipPercentage);
+
+    const tip = Number('.' + tipPercentage) * total;
+
+    response.send(`${tip}`);
 });
 
 app.listen(3000, () => {
